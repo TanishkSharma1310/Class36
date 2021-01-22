eb101cf0dc1091f4d177a55e0e6ad8d3d51cb6f2
TanishkSharma1310 <72088554+TanishkSharma1310@users.noreply.github.com>
2021-01-22 20:00:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var canvas,bgimg;
+var playerCount,form,player,game,database;
+var gameState = 0;
+
+function setup(){
+
+  canvas = createCanvas(500,500);
+database = firebase.database();
+
+}
+
+function draw(){
+  background("white");
+  
+    
+}
